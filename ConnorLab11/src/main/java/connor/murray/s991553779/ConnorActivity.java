51691d501af34ac91a1917b29887a4f12fc19249
HackdaNorth <36301205+HackdaNorth@users.noreply.github.com>
2021-01-25 20:21:36 -0500

    fourth commit, added two buttons, added temp functionality. - Changed color of first acitivity screen - Added new title screen. - Added switch but not functionality yet - Added text view for user to add text.

--- ConnorLab11/src/main/java/connor/murray/s991553779/ConnorActivity.java
+++ ConnorLab11/src/main/java/connor/murray/s991553779/ConnorActivity.java
@@ -5,7 +5,9 @@ This is assignment 1 to demostrate my performce of activities  and how to invoke
 package connor.murray.s991553779;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 import android.os.Bundle;
 
 public class ConnorActivity extends AppCompatActivity {
@@ -14,5 +16,20 @@ public class ConnorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void callIntent(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btnLaunch:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.twitch.tv"));
+                startActivity(intent);
+                break;
+            case R.id.btnSnack:
+                intent = new Intent(this, MurrayActivity.class);
+                startActivity(intent);
+            default:
+                break;
+        }
     }
 }
