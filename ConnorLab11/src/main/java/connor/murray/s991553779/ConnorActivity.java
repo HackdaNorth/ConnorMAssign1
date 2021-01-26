c57cb9fdcfbd0a38bb19f63df3d4c9a905385a50
HackdaNorth <36301205+HackdaNorth@users.noreply.github.com>
2021-01-25 19:05:56 -0500

    first commit. Nothing new,

---
+++ ConnorLab11/src/main/java/connor/murray/s991553779/ConnorActivity.java
@@ -0,0 +1,18 @@
+/*
+Connor Murray 991553779
+This is assignment 1 to demostrate my performce of activities  and how to invoke third party app
+ */
+package connor.murray.s991553779;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
